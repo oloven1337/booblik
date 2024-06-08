5527de17773677417089ef3e7b685b32ea29b67b
Ibragim <oloven1337@gmail.com>
2024-06-08 16:59:03 +0600

    redirect

--- src/AppRedirect.jsx
+++ src/AppRedirect.jsx
@@ -3,18 +3,18 @@ import { isIOS, isAndroid } from 'react-device-detect';
 
 const AppRedirect = () => {
     useEffect(() => {
-        const androidScheme = 'your-android-app-scheme://'; // замените на схему вашего Android приложения
-        const iosScheme = 'your-ios-app-scheme://'; // замените на схему вашего iOS приложения
-        const playStoreUrl = 'https://play.google.com/store/apps/details?id=your.android.app.id'; // замените на URL вашего Android приложения в Play Market
-        const appStoreUrl = 'https://apps.apple.com/us/app/your-ios-app-id'; // замените на URL вашего iOS приложения в App Store
+        // const androidScheme = 'your-android-app-scheme://';
+        // const iosScheme = 'your-ios-app-scheme://'; //
+        const playStoreUrl = 'https://play.google.com/store/apps/details?id=com.bublik.app&hl=ru&gl=US'; // замените на URL вашего Android приложения в Play Market
+        const appStoreUrl = 'https://apps.apple.com/us/app/%D0%BA%D0%BE%D1%84%D0%B5%D0%B9%D0%BD%D1%8F-%D0%B1%D1%83%D0%B1%D0%BB%D0%B8%D0%BA/id1481448597'; // замените на URL вашего iOS приложения в App Store
 
         if (isAndroid) {
-            window.location.href = androidScheme;
+            // window.location.href = androidScheme;
             setTimeout(() => {
                 window.location.href = playStoreUrl;
             }, 250);
         } else if (isIOS) {
-            window.location.href = iosScheme;
+            // window.location.href = iosScheme;
             setTimeout(() => {
                 window.location.href = appStoreUrl;
             }, 250);

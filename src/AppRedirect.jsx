d1408f1007ce06dea3fae0b694b2ba4ba9ebc104
Ibragim <oloven1337@gmail.com>
2024-06-20 08:41:06 +0600

    redirect

--- src/AppRedirect.jsx
+++ src/AppRedirect.jsx
@@ -4,26 +4,37 @@ import { isIOS, isAndroid } from 'react-device-detect';
 const AppRedirect = () => {
     useEffect(() => {
         const playStoreUrl = 'https://play.google.com/store/apps/details?id=kg.geekstudio.booblik.android';
+        const appStoreUrl = 'https://apps.apple.com/us/app/%D0%BA%D0%BE%D1%84%D0%B5%D0%B9%D0%BD%D1%8F-%D0%B1%D1%83%D0%B1%D0%BB%D0%B8%D0%BA/id1481448597';
+
+        const androidAppUrl = 'intent://kg.geekstudio.booblik.android#Intent;scheme=kg.geekstudio.booblik.android;package=kg.geekstudio.booblik.android;end';
+        const iosAppUrl = 'booblik://';
 
         function isStandaloneMode() {
             return ('standalone' in window.navigator) && (window.navigator.standalone);
         }
 
-        // Проверяем, является ли запрос универсальной ссылкой
         function isUniversalLink() {
             return document.referrer.includes('apple-app-site-association');
         }
 
-        if (isAndroid) {
+        const openApp = (url, fallbackUrl) => {
+            const start = new Date().getTime();
+            window.location.href = url;
+
             setTimeout(() => {
-                window.location.href = playStoreUrl;
-            }, 250);
-        } else if (isIOS) {
-            setTimeout(() => {
-                if (!isStandaloneMode() && !isUniversalLink()) {
-                    window.location.href = 'https://apps.apple.com/us/app/%D0%BA%D0%BE%D1%84%D0%B5%D0%B9%D0%BD%D1%8F-%D0%B1%D1%83%D0%B1%D0%BB%D0%B8%D0%BA/id1481448597';
+                const end = new Date().getTime();
+                if (end - start < 1500) {
+                    window.location.href = fallbackUrl;
                 }
-            }, 250);
+            }, 1000);
+        };
+
+        if (isAndroid) {
+            openApp(androidAppUrl, playStoreUrl);
+        } else if (isIOS) {
+            if (!isStandaloneMode() && !isUniversalLink()) {
+                openApp(iosAppUrl, appStoreUrl);
+            }
         }
     }, []);
 

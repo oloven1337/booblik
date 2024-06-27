7a658eb99bc431e55d28d691098e9d61e717c7a9
Ibragim <oloven1337@gmail.com>
2024-06-27 16:32:54 +0600

    redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import './App.css'
 
 import AppRedirect from "./AppRedirect.jsx";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+
+export const routes= [
+    {
+        path: '*',
+        element: null,
+    },
+];
+
 
 function App() {
+    const router = createBrowserRouter(routes);
 
   return (
     <>
+        <RouterProvider router={router} />
         <AppRedirect />
       <div>redirect to app</div>
     </>
